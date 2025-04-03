82548c69de33e7de443e946f234c23135f25371b
Lucas Avelino <lugabriell084@gmail.com>
2025-04-02 23:33:23 -0300

    Feat: Atualizei o Header

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {link} from "react-router-dom";
 import Style from "./Header.module.css"
 
 function Header(){
@@ -8,7 +9,8 @@ function Header(){
          <h1>Eventos</h1>
           <nav>
               <link to="/"> Home </link>
-              <link to="/eventos"> eventos </link>
+              <link to="/eventos"> Eventos </link>
+              <link to="/cadastrar"> Cadastrar Evento </link>
           </nav>
 
         </div>
